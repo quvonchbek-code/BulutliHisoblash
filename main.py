cfd688f4f4574f372b9bc138075c3231a020f1b3
Quvonchbek <boboxonovmirkomil900@gmail.com>
2025-12-04 11:38:21 +0500

    2lik va 8lik bajarildi

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ def dec_to_bin(n):
         x //= 2
     return res
 
+def dec_to_oct(n):
+    res = ""
+    x = n
+    while x > 0:
+        res = str(x % 8) + res
+        x //= 8
+    return res
+
+
 
 n = int(input("10-lik son kiriting: "))
 
 print("2-lik:", dec_to_bin(n))
+print("8-lik:", dec_to_oct(n))
