012e176e930d812f7f4d1edee3b7bd65297e78d4
Quvonchbek <boboxonovmirkomil900@gmail.com>
2025-12-04 11:40:59 +0500

    10 ham qoshildi

--- main.py
+++ main.py
@@ -14,9 +14,17 @@ def dec_to_oct(n):
         x //= 8
     return res
 
-
+def dec_to_hex(n):
+    HEX = "0123456789ABCDEF"
+    res = ""
+    x = n
+    while x > 0:
+        res = HEX[x % 16] + res
+        x //= 16
+    return res
 
 n = int(input("10-lik son kiriting: "))
 
 print("2-lik:", dec_to_bin(n))
 print("8-lik:", dec_to_oct(n))
+print("16-lik:", dec_to_hex(n))
